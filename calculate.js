2d779275fe7c33407497e027be0526877764c3fa
lukesaltweather <andilengl@outlook.de>
2016-05-23 16:16:02 +0200

    Bugfix

--- calculate.js
+++ calculate.js
@@ -2,7 +2,7 @@ var Calculate = function(phrase) {
   var sentence = phrase.toLowerCase();
   if(sentence != null){
     $.ajax({
-  url: 'https://api.wit.ai/converse?v=20160523',
+  url: 'https://api.wit.ai/converse',
   data: {
     'q': sentence,
     'access_token' : 'XZXOIWC5TRUS6LIYQ7WRAC3ZZZU2RCRK'
