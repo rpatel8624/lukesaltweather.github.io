b014faf210950ad0ef42bfdfd0251f5253889a30
lukesaltweather <andilengl@outlook.de>
2016-05-23 16:13:40 +0200

    Bugfix

--- calculate.js
+++ calculate.js
@@ -2,13 +2,13 @@ var Calculate = function(phrase) {
   var sentence = phrase.toLowerCase();
   if(sentence != null){
     $.ajax({
-  url: 'https://api.wit.ai/converse',
+  url: 'https://api.wit.ai/converse?v=20160523',
   data: {
     'q': sentence,
     'access_token' : 'XZXOIWC5TRUS6LIYQ7WRAC3ZZZU2RCRK'
   },
   dataType: 'jsonp',
-  method: 'GET',
+  method: 'POST',
   success: function(response) {
       console.log("success!", response);
   }
