bfb5fe077989c26f9103fea9ff6abec435e482ff
lukesaltweather <andilengl@outlook.de>
2016-05-26 17:12:32 +0200

    updated Token

--- en/messages.js
+++ en/messages.js
@@ -2,4 +2,4 @@
 var errmsg = "Fatal Error detected. Please contact Admin"; //Message appears when an error happens
 var recordmsg = "Recording";
 var Init_Message = "Initalising Samaritan ... Samaritan initiated. Welcome to the open beta. What are your commands ?";
-var contoken = "XZXOIWC5TRUS6LIYQ7WRAC3ZZZU2RCRK";
+var contoken = "4IRORWB7ZUSSC3ZFA7HR4FU4W53TVZIZ";
