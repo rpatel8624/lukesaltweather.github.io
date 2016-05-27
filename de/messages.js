8aeee40236e03723f8440cb1acb5b2596cb4f343
lukesaltweather <andilengl@outlook.de>
2016-05-27 17:50:09 +0200

    Update messages.js

--- de/messages.js
+++ de/messages.js
@@ -3,3 +3,4 @@ var errmsg = "Ein Fehler trat auf. Kontaktiere Admin..."; //Message appears when
 var recordmsg = "Aufnahme";
 var Init_Message = "Initalisiere Samaritan ... Samaritan initiert. Willkommen zu Version 1. wie lauten deine Befehle ?";
 var contoken = "XZXOIWC5TRUS6LIYQ7WRAC3ZZZU2RCRK";
+var calmsg = "Berechne Antwort"
