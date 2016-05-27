666dbad1130c3e6079df41123a706ef91ef0bbaa
lukesaltweather <andilengl@outlook.de>
2016-05-27 17:50:48 +0200

    Update messages.js

--- en/messages.js
+++ en/messages.js
@@ -3,3 +3,4 @@ var errmsg = "Fatal Error detected. Please contact Admin"; //Message appears whe
 var recordmsg = "Recording";
 var Init_Message = "Initalising Samaritan ... Samaritan initiated. Welcome to v1. What are your commands ?";
 var contoken = "4IRORWB7ZUSSC3ZFA7HR4FU4W53TVZIZ";
+var calmsg = "Calculating response";
