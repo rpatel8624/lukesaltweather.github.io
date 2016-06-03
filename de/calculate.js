473ff7aadc5793a0ef8d4ee69ec9b7b7de9de39f
lukesaltweather <andilengl@outlook.de>
2016-06-03 22:24:53 +0200

    Update calculate.js

--- de/calculate.js
+++ de/calculate.js
@@ -1,22 +1,24 @@
 var Calculate = function (phrase) {
     if (phrase != null) {
-            $.ajax({
+            var intent = $.ajax({
                 type: "POST",
                 url: baseUrl + "query/",
                 contentType: "application/json; charset=utf-8",
                 dataType: "json",
+                global: false,
+                async: false,
                 headers: {
                     "Authorization": "Bearer " + apiToken
                 },
                 data: JSON.stringify({ q: phrase, lang: "de" }),
                 success: function (data) {
-                    intent = data;
-                     console.log(intent);
+                    return data;
                 },
                 error: function () {
                     executeSamaritan("Ein fehler trat auf");
                 }
-            });
+            }).responseText;
+            console.log(intent);
     }
     else {
         answer = "Ein Fehler trat auf .";
