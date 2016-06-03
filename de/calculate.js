1763c6f27d89fd3e20f1aa62eef87bbd68231272
lukesaltweather <andilengl@outlook.de>
2016-06-03 21:30:06 +0200

    Road to V2

--- de/calculate.js
+++ de/calculate.js
@@ -1,29 +1,25 @@
-var Calculate = function(phrase) {
-  if(phrase != null){ 
-    phrase = phrase.toLowerCase();
-    switch(phrase){
-    case "guten tag":
-    case "servus" :
-    case "hallo" :
-      var answer = ["guten tag .", "hallo .", "willkommen"];
-	  var randomNumber = Math.floor(Math.random()*answer.length);
-      return answer[randomNumber];
-    
-    case "wie lauten deine befehle" :
-      var answer = " finde die maschine .";
-      return answer;
-  
-    case "wie geht es dir" :
-      var answer = " Sehr gut, und dir ?"
-      return answer;
-   default:
-      answer = " ich verstehe nicht";
-       return answer;
-  
-  }
-  }
-  else{
-    answer = "Ein Fehler trat auf .";
-    return answer;
-  }
+var Calculate = function (phrase) {
+    if (phrase != null) {
+            $.ajax({
+                type: "POST",
+                url: baseUrl + "query/",
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                headers: {
+                    "Authorization": "Bearer " + apiToken
+                },
+                data: JSON.stringify({ q: phrase, lang: "de" }),
+                success: function (data) {
+                    var intent = data;
+                },
+                error: function () {
+                    executeSamaritan("Ein fehler trat auf");
+                }
+            });
+            console.log(intent);
+    }
+    else {
+        answer = "Ein Fehler trat auf .";
+        return answer;
+    }
 }
